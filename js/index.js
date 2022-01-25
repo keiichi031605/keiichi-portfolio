9367a5ee469d602f910f33b2f9f0ce9431bc34e5
Keiichi Katsuno <keiichikatsuno@Keiichis-MBP.gateway>
2022-01-25 16:58:19 +1100

    commit

--- js/index.js
+++ js/index.js
@@ -1,46 +1,46 @@
  // Navbar Scroll-Effect
- const navbar = document.querySelector('#navbar-wrapper');
- window.onscroll = () => {
-   if (document.documentElement.scrollTop >= 50 ) {
-     navbar.style.background = "black";
-     navbar.style.transition = '0.8s ease'
-   }
-   else {
-     navbar.style.background = "transparent";
-   }
- };
+  const navbar = document.querySelector('#navbar-wrapper');
+  window.onscroll = () => {
+    if (document.documentElement.scrollTop >= 50 ) {
+      navbar.style.background = "black";
+      navbar.style.transition = '0.8s ease'
+    }
+    else {
+      navbar.style.background = "transparent";
+    }
+  };
 
- // Automated update Month and Year in footer
- const update = () => {
-   const newDate = new Date();
-   const year = newDate.getFullYear();
-   const month = newDate.getMonth() + 1;
-   const months = ['January', 'February', 'March',
-   'April', 'May', 'June', 'July',
-   'August', 'September', 'October',
-   'November', 'December'
-   ];
+  // Automated update Month and Year in footer
+  const update = () => {
+    const newDate = new Date();
+    const year = newDate.getFullYear();
+    const month = newDate.getMonth() + 1;
+    const months = ['January', 'February', 'March',
+    'April', 'May', 'June', 'July',
+    'August', 'September', 'October',
+    'November', 'December'
+    ];
 
-   for (let i = 0; i < months.length; i++) {
-     if ((i + 1) === month) {
-       document.querySelector('.footer-copyright').innerText = `Last update ${months[i]} ${year}`;
-       break;
-     };
-   };
- };
+    for (let i = 0; i < months.length; i++) {
+      if ((i + 1) === month) {
+        document.querySelector('.footer-copyright').innerText = `Last update ${months[i]} ${year}`;
+        break;
+      };
+    };
+  };
 
- // mobile navbar
- const navSlide = () => {
-   const burger = document.querySelector('.burger');
-   const nav = document.querySelector('.navbar-list');
-   const navbar = document.querySelector('#navbar-wrapper');
+  // mobile navbar
+  const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar-list');
+    const navbar = document.querySelector('#navbar-wrapper');
 
-   burger.addEventListener('click', () => {
-     nav.classList.toggle('nav-active');
-     navbar.style.background = "black";
-     burger.classList.toggle('toggle');
-   });
- };
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      navbar.style.background = "black";
+      burger.classList.toggle('toggle');
+    });
+  };
 
 
 navSlide();
